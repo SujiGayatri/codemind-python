0e3ac0183552fdd68b14836dd68794fb3b6b916f
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-05-08 10:39:12 +0530

    x=int(input()) for i in range(x):     n=int(input())     a=list(map(int,input().split()))     k=[]     for i in a:         k.append(i)     a.sort()     if k==a:         print("0")     else:         print(max(a)-min(a))

---
+++ Optimal_Sorting.py
@@ -0,0 +1,12 @@
+x=int(input())
+for i in range(x):
+    n=int(input())
+    a=list(map(int,input().split()))
+    k=[]
+    for i in a:
+        k.append(i)
+    a.sort()
+    if k==a:
+        print("0")
+    else:
+        print(max(a)-min(a))
