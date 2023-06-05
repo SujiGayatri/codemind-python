6608a02042f0402e8e6b8facc82b69cca7b084e2
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-06-05 23:11:39 +0530

    n=int(input()) c=0 x=0 a=0 for i in range (1,n+1,1):     if(n%i==0):         c+=1 if(c==2):     while(n!=0):         d=n%10         n=n//10         s=0         for j in range (1,d+1,1):             if(d%j==0):                 s+=1         if(s==2):             x+=1         a+=1 if(x==a and c==2):     print('Mega Prime') elif(c>2 or x!=a):     print('Not Mega Prime')

--- mega_prime.py
+++ mega_prime.py
@@ -1,24 +1,22 @@
 n=int(input())
-f=0
-d=0
-p=0
-for i in range(1,n+1):
-    if n%i==0:
-        f+=1
-if f==2:
-    while n>0:
-        f=0
-        m=n%10
-        for j in range(1,m+1):
-            if m%j==0:
-                f+=1
-        if f==2:
-            p+=1
-        d+=1
+c=0
+x=0
+a=0
+for i in range (1,n+1,1):
+    if(n%i==0):
+        c+=1
+if(c==2):
+    while(n!=0):
+        d=n%10
         n=n//10
-    if d==p:
-        print("Mega Prime")
-    else:
-        print("Not Mega Prime")
-else:
-    print("Not Mega Prime")
+        s=0
+        for j in range (1,d+1,1):
+            if(d%j==0):
+                s+=1
+        if(s==2):
+            x+=1
+        a+=1
+if(x==a and c==2):
+    print('Mega Prime')
+elif(c>2 or x!=a):
+    print('Not Mega Prime')
